e9af8d8a781d989679d23edc0fde7395b1cdb941
Matrixoid <mr.art.pro@mail.ru>
2023-11-24 23:40:18 +0300

    DDL.sql for hw2 updated

--- hw2/src/DDL.sql
+++ hw2/src/DDL.sql
@@ -7,7 +7,7 @@ CREATE TABLE Students (
 	student_id INT PRIMARY KEY,
 	student_name VARCHAR(50) NOT NULL,
 	group_id INT NOT NULL,
-    UNIQUE (student_id, group_id),
+	UNIQUE (student_id, group_id),
 	FOREIGN KEY (group_id) REFERENCES Groups (group_id)
 );
 CREATE TABLE Lecturers (
@@ -19,20 +19,20 @@ CREATE TABLE Courses (
 	course_name varchar(50) NOT NULL
 );
 CREATE TABLE Plans (
-    course_id INT NOT NULL,
-    group_id INT NOT NULL,
-    lecturer_id INT NOT NULL,
-    PRIMARY KEY (course_id, group_id),
+	course_id INT NOT NULL,
+	group_id INT NOT NULL,
+	lecturer_id INT NOT NULL,
+	PRIMARY KEY (course_id, group_id),
 	FOREIGN KEY (group_id) REFERENCES  Groups (group_id),
-    FOREIGN KEY (course_id) REFERENCES  Courses (course_id),
-    FOREIGN KEY (lecturer_id) REFERENCES  Lecturers (lecturer_id)
+	FOREIGN KEY (course_id) REFERENCES  Courses (course_id),
+	FOREIGN KEY (lecturer_id) REFERENCES  Lecturers (lecturer_id)
 );
 CREATE TABLE Marks (
-    student_id INT NOT NULL,
-    group_id INT NOT NULL,
-    course_id INT NOT NULL,
-    points INT NOT NULL CHECK (points >= 0 AND points <= 100),
-    PRIMARY KEY (student_id, group_id, course_id),
+	student_id INT NOT NULL,
+	group_id INT NOT NULL,
+	course_id INT NOT NULL,
+	points INT NOT NULL CHECK (points >= 0 AND points <= 100),
+	PRIMARY KEY (student_id, group_id, course_id),
 	FOREIGN KEY (group_id, course_id) REFERENCES  Plans (group_id, course_id),
-    FOREIGN KEY (student_id, group_id) REFERENCES Students (student_id, group_id)
+	FOREIGN KEY (student_id, group_id) REFERENCES Students (student_id, group_id)
 );
